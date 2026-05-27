a99c884d1274aa9a841575d25c67eb8f25f27471
ArtfulArtie <259197527+ArtfulArtie@users.noreply.github.com>
2026-05-26 23:59:54 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ copyright.innerHTML = `\u00A9 Arthur Osorio ${thisYear}`;
 footer.appendChild(copyright);
 
 let skills = ["JavaScript", "HTML", "CSS", "Adobe Creative Cloud", "Github", "Autodesk Maya", "Mudbox", "GIMP", "Blender", "Canva", "Photography"];
-const skillsSelection = document.querySelector('#Skills');
+const skillsSelection = document.querySelector('#skills');
 const skillsList = skillsSelection.querySelector('ul');
 for(let i=0 ; i< skills.length ; i++){
     let skill = document.createElement('li');
@@ -22,3 +22,37 @@ for(let i=0 ; i< skills.length ; i++){
     skillsList.appendChild(skill);
 
 }
+
+const messageForm = document.querySelector('form[name="leave_message"]')
+messageForm.addEventListener("submit", function(event) {
+
+event.preventDefault();
+const usersName = event.target.usersName.value ;
+const usersEmail = event.target.usersEmail.value ;
+const usersMessage = event.target.usersMessage.value ;
+
+console.log(usersName, usersEmail, usersMessage);
+
+const messageSection = document.querySelector('#messages');
+const messageList = messageSection.querySelector('ul');
+const newMessage = document.createElement('li');
+newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+<span>${usersMessage}</span>`
+
+const removeButton = document.createElement('button');
+removeButton.innerText= 'Remove';
+removeButton.type = 'button';
+
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
+
+removeButton.addEventListener("click", function(event){
+const entry = event.target.parentNode;
+entry.remove();
+
+});
+
+messageForm.reset();
+});
+
+
